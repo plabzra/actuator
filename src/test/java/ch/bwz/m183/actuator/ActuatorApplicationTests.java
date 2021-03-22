e59f2d834cd480de6b65d05e266169898182e370
plbzra <73287266+plabzra@users.noreply.github.com>
2021-03-22 18:29:36 +0100

    init

---
+++ src/test/java/ch/bwz/m183/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bwz.m183.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

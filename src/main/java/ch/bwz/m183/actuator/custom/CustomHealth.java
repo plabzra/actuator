c2762bc9e9dd9077da2ca6f6bde98e1939246d51
plbzra <73287266+plabzra@users.noreply.github.com>
2021-03-25 13:42:30 +0100

    add body in custom health endpoint

--- src/main/java/ch/bwz/m183/actuator/custom/CustomHealth.java
+++ src/main/java/ch/bwz/m183/actuator/custom/CustomHealth.java
@@ -1,4 +1,23 @@
 package ch.bwz.m183.actuator.custom;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class CustomHealth {
+
+    private final Map<String, Object> healthDetails = new HashMap<>();
+
+    @JsonAnyGetter
+    public Map<String, Object> getHealthDetails() {
+        return this.healthDetails;
+    }
+
+    public void setHealthDetails(Map<String, Object> details) {
+        this.healthDetails.putAll(details);
+    }
+
 }
